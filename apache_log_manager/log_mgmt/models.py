f231e831188cdcc515267bead6e67e33fbe8deec
TunaLagoona <maslova.wrk@gmail.com>
2021-04-19 19:43:37 +0100

    Adjust Parser model

--- apache_log_manager/log_mgmt/models.py
+++ apache_log_manager/log_mgmt/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Parser(models.Model):
     ip_address = models.CharField('IP address', max_length=200)
-    log_data = models.CharField(max_length=200)
+    log_date = models.DateTimeField('log date')
     http_method = models.CharField(max_length=200)
     uri = models.CharField(max_length=200)
-    response_code = models.CharField(max_length=200)
+    response_code = models.IntegerField('response code')
     response_size = models.CharField(max_length=200)
-
-    # models.DateTimeField('date published')
+    referer = models.CharField('Referer', max_length=200)
+    user_agent = models.CharField('User Agent', max_length=200)
