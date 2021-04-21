754a713dcf615388602826ccb9e6ebfa76f982fc
TunaLagoona <maslova.wrk@gmail.com>
2021-04-21 12:56:59 +0100

    Make list and input filters

--- apache_log_manager/log_mgmt/admin.py
+++ apache_log_manager/log_mgmt/admin.py
@@ -2,4 +2,29 @@ from django.contrib import admin
 
 from .models import Parser
 
-admin.site.register(Parser)
+
+class ParserAdmin(admin.ModelAdmin):
+    list_display = (
+        "ip_address",
+        "log_date",
+        "HTTP_method",
+        "URI",
+        "response_code",
+        "response_size",
+        "referer",
+        "user_agent",
+    )
+    list_filter = ("response_code", "HTTP_method", "log_date")
+    search_fields = (
+        "ip_address",
+        "log_date",
+        "HTTP_method",
+        "URI",
+        "response_code",
+        "response_size",
+        "referer",
+        "user_agent",
+    )
+
+
+admin.site.register(Parser, ParserAdmin)
