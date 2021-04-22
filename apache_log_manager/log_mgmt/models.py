d19e4dbe2e3f654affc11508d354db3ef87c7387
TunaLagoona <maslova.wrk@gmail.com>
2021-04-22 12:58:44 +0100

    Create Indexes for better performance

--- apache_log_manager/log_mgmt/models.py
+++ apache_log_manager/log_mgmt/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class ApacheLogs(models.Model):
     ip_address = models.CharField("IP address", max_length=200)
-    log_date = models.DateTimeField("Log date", default=None)
-    HTTP_method = models.CharField(max_length=200)
-    URI = models.CharField(max_length=200)
-    response_code = models.IntegerField("Response code")
+    log_date = models.DateTimeField("Log date", default=None, db_index=True)
+    HTTP_method = models.CharField(max_length=200, db_index=True)
+    URI = models.CharField(max_length=700)
+    response_code = models.IntegerField("Response code", db_index=True)
     response_size = models.CharField(max_length=200)
-    referer = models.CharField("Referer", max_length=200, default=None)
-    user_agent = models.CharField("User Agent", max_length=200, default=None)
+    referer = models.CharField("Referer", max_length=400, default=None)
+    user_agent = models.CharField("User Agent", max_length=400, default=None)
